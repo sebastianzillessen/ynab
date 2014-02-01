6b43d76f999990445e70b8463894f80bfcffab1b
theorician <theorician@me.com>
2014-01-31 18:04:19 -0700

    add ccstatement and HCBI fetching scripts.

---
+++ lib/ccstatement.rb
@@ -0,0 +1,91 @@
+require 'rubygems'
+require 'mechanize'
+require 'csv'
+require './qif'
+
+# This code is modified from leoc's ledgit credit card handler. 
+# https://github.com/leoc/ledgit/blob/876fc22137dc640dd5116bc7caf9386fbbc41f3c/lib/handler/dkb/creditcard.rb
+
+class CreditStatement
+  def self.run!
+    $stderr.print "Username: "; username = gets.strip
+    $stderr.print "Password: "; password = gets.strip
+    $stderr.print "Account: ";  account = gets.strip
+
+    cc = CreditStatement.new(username, password, account)
+    puts Qif.print cc.statement, 'CCard'
+  end
+  
+  def initialize(username, password, account)
+    @agent = Mechanize.new
+    @username = username
+    @password = password
+    @account = account
+  end
+  
+  def statement
+    @login ||= login
+    @statement ||= parse(download_data)
+  end
+  
+  private
+  
+  def parse data
+    data.encode!('UTF-8', 'ISO-8859-1')
+    data.gsub!(/\A.*\n\n.*\n\n/m, '')
+
+    result = CSV.parse(data, col_sep: ';', headers: :first_row)
+    result.map do |row|
+      {
+        :date   => Date.parse(row['Belegdatum']),
+        :amount => row['Betrag (EUR)'].gsub('.', '').gsub(',', '.').to_f,
+        :payee  => row['Umsatzbeschreibung'],
+        :memo   => row[5]
+      }
+    end.reverse
+  end
+
+  
+  def name_for_label label_text
+    @agent.page.labels.select { |l| l.text =~ /#{label_text}/ }
+      .first.node.attribute('for').value
+  end
+
+  def download_data
+    form = @agent.page.forms[2]
+
+    posting_date = (Date.today - 180).strftime('%d.%m.%Y')
+    to_posting_date = Date.today.strftime('%d.%m.%Y')
+
+    form.field_with(name: /slCreditCard/)
+      .option_with(text: /#{Regexp.escape(@account)}/).select
+
+    form.radiobutton_with(name: /searchPeriod/, value: '0').check
+    form.field_with(name: 'postingDate').value = posting_date
+    form.field_with(name: 'toPostingDate').value = to_posting_date
+    form.submit
+
+    @agent.page.link_with(href: /csvExport/).click
+    @agent.page.body
+  end
+
+  def login
+    # log into the online banking website
+    @agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    @agent.get 'https://banking.dkb.de:443/dkb/-?$javascript=disabled'
+    form = @agent.page.forms.first
+
+    form.field_with(name: name_for_label(/Anmeldename/)).value = @username
+    form.field_with(name: name_for_label(/PIN/)).value = @password
+
+    button = form.button_with(value: /Anmelden/)
+
+    @agent.submit(form, button)
+
+    # go to the transaction listing for the correct account type
+    @agent.page.link_with(text: /Kreditkartenumsätze/).click
+    @agent.page.meta_refresh.first.click unless @agent.page.meta_refresh.empty?
+  end
+end
+
+CreditStatement.run! if __FILE__==$0
---
+++ lib/hcbifetcher.rb
@@ -0,0 +1,79 @@
+# Ruby 1.9.2
+require 'csv'
+require './qif'
+
+class HCBIParser
+  def self.run!
+    hcbi = HCBIParser.new
+    puts Qif.print hcbi.parse, 'Bank'
+  end
+
+  def parse
+    parsed_csv.map do |row|
+      {
+        :date        => Date.parse(row[:cleared_date]),
+        :currency    => row[:currency],
+        :amount      => row[:amount],
+        :payee       => row[:payee],
+        :memo        => row_memo(row)
+      }
+    end
+  end
+    
+  private
+  
+  def row_memo row
+    memo = ""
+    memo += "BLZ: #{row[:sort_code]} "      unless row[:sort_code].empty?
+    memo += "KTO: #{row[:account_number]} " unless row[:account_number].empty?
+    memo += "Memo: #{row[:memo]} "          unless row[:memo].empty?
+  end
+    
+  def parsed_csv
+    csv_array = CSV.parse reformatted_csv
+    parsed_array = csv_array.map do |row|
+      parsed_row row
+    end
+  end
+  
+  def reformatted_csv
+    reformatted = []
+    CSV.parse(fetch_csv, col_sep: ";") do |rec|
+      reformatted.push CSV.generate_line(rec, col_sep: ",", force_quotes: true)
+    end
+    reformatted.shift
+    reformatted.join
+  end
+  
+  def parsed_row row_array
+    {
+      :sort_code      => row_array[3].to_s.strip,
+      :account_number => row_array[4].to_s.strip,
+      :currency       => row_array[8].to_s.strip,
+      :amount         => row_array[7].to_s.strip,
+      :booking_date   => row_array[6].to_s.strip,
+      :cleared_date   => row_array[5].to_s.strip,
+      :payee          => row_array[10..11].join.strip,
+      :memo           => row_array[12..23].join(" ").strip,
+    }
+  end
+
+  def fetch_csv
+    # To re-create the local aqbanking config files: 
+    # aqhbci-tool4 adduser -s https://hbci-pintan-by.s-hbci.de/PinTanServlet -b BLZ -u KTONR -N ANYNAME -t pintan
+    # aqhbci-tool4 adduserflags -f forceSsl3
+    # aqhbci-tool4 getsysid
+    # aqhbci-tool4 listaccounts
+    # aqbanking-cli request --balance
+    
+    # Other common commands: 
+    # aqbanking-cli request --transactions
+    # aqbanking-cli request --transactions > transaktionen.ctx
+    # aqbanking-cli listtrans < transaktionen.ctx
+    `aqbanking-cli -n -P ~/.aqbanking/pin request --transactions | aqbanking-cli listtrans`
+  end
+end
+
+HCBIParser.run! if __FILE__==$0
+
+
